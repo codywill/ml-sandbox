193e6b4ef5404cfe7771043a46fca1fd552011b0
Cody Williams <codywill77@gmail.com>
2019-12-25 15:36:59 -0800

    Replaced find-s.py with algorithms.py and demo.py
    Keeps algorithms in one place, enables import

--- algorithms.py
+++ algorithms.py
@@ -1,19 +1,6 @@
 import numpy as np
 import pandas as pd
 
-# Arbitrary training data
-df_a = pd.DataFrame(np.random.randint(0, 2, size=(5, 5)), columns=list('ABCDE'))
-
-# EnjoySport example
-columns = ['Sky', 'AirTemp', 'Humidity', 'Wind', 'Water', 'Forecast', 'EnjoySport']
-samples = [
-    ['Sunny','Warm','Normal','Strong','Warm','Same',1],
-    ['Sunny','Warm','High','Strong','Warm','Same',1],
-    ['Rainy','Cold','High','Strong','Warm','Change',0],
-    ['Sunny','Warm','High','Strong','Cool','Change',1]
-]
-df_b = pd.DataFrame(samples, columns=columns)
-
 # Update specific hypothesis s using sample of training data d
 def update_s(s, d):
     for key in s.keys():
@@ -35,9 +22,3 @@ def find_s(df):
         if row.iloc[-1] == 1:
             s = update_s(s, row)
     return s
-
-print(df_a)
-print(find_s(df_a))
-print('\n')
-print(df_b)
-print(find_s(df_b))
---
+++ demo.py
@@ -0,0 +1,22 @@
+import algorithms as ag
+import pandas as pd
+import numpy as np
+
+# Arbitrary training data
+df_a = pd.DataFrame(np.random.randint(0, 2, size=(5, 5)), columns=list('ABCDE'))
+
+# EnjoySport example
+columns = ['Sky', 'AirTemp', 'Humidity', 'Wind', 'Water', 'Forecast', 'EnjoySport']
+samples = [
+    ['Sunny','Warm','Normal','Strong','Warm','Same',1],
+    ['Sunny','Warm','High','Strong','Warm','Same',1],
+    ['Rainy','Cold','High','Strong','Warm','Change',0],
+    ['Sunny','Warm','High','Strong','Cool','Change',1]
+]
+df_b = pd.DataFrame(samples, columns=columns)
+
+print(df_a)
+print(ag.find_s(df_a))
+print('\n')
+print(df_b)
+print(ag.find_s(df_b))
